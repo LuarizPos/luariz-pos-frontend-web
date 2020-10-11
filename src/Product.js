fb58c6650315eac05c619c597192908e0f1c34ec
wicaksana94 <wicaksana94@gmail.com>
2020-10-11 14:45:21 +0700

    Make height of product card more dynamic

--- src/Product.js
+++ src/Product.js
@@ -3,22 +3,24 @@ import "./Product.css";
 
 function Product(props) {
   return (
-    <div className="product">
-      <div className="product__card card m-3">
-        <div className="card-body d-flex flex-column justify-content-between">
-          <div className="card-title">
-            <label className="product__title">{props.title}</label>
-            <label className="product__description">{props.description}</label>
-          </div>
-          <div className="d-block">
-            <div className="d-flex justify-content-between">
-              <div className="product__price">
-                <h6>Rp {props.price}</h6>
-              </div>
+    <div className="product" id={props.id} key={props.key}>
+      <div className="product__card card m-2">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-7">
+              <h6 className="product__title">{props.title}</h6>
+              <label className="product__description">
+                {props.description}
+              </label>
+            </div>
+            <div className="col-5">
               <div className="product__image">
                 <img src={props.image} alt="" className="rounded" />
               </div>
             </div>
+          </div>
+          <div className="product__price">
+            <h6>Rp {props.price}</h6>
           </div>
         </div>
       </div>
