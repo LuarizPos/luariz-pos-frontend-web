1587186dcf9314b0ba581a18432aa2620e9b1ad1
wicaksana94 <wicaksana94@gmail.com>
2020-12-14 17:38:43 +0700

    Add Category reducer

---
+++ src/store/reducers/categoryReducer.js
@@ -0,0 +1,25 @@
+import { GET_CATEGORIES, CATEGORIES_ERROR, UPDATE_CATEGORIES } from "../types";
+
+const initialState = {
+  categories: [],
+  loading: true,
+};
+
+export default function categoryReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload,
+        loading: false,
+      };
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
