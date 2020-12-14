9a67b764f685b17933d93c796c02219ff88bb447
wicaksana94 <wicaksana94@gmail.com>
2020-12-14 17:39:41 +0700

    Combine the reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import productReducer from "./productReducer";
+import categoryReducer from "./categoryReducer";
 
 export default combineReducers({
+  categories: categoryReducer,
   products: productReducer,
   users: userReducer,
 });
