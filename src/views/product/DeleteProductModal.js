d901255a6973e418fd273ffab731a414545fe4fb
wicaksana94 <wicaksana94@gmail.com>
2020-12-15 19:51:06 +0700

    Add delete product modal

--- src/views/product/DeleteProductModal.js
+++ src/views/product/DeleteProductModal.js
@@ -6,17 +6,52 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DeleteIcon from "@material-ui/icons/Delete";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  deleteProducts,
+  showLoading,
+  hideLoading,
+  clearError,
+} from "../../store/actions/productsActions";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Alert from "@material-ui/lab/Alert";
 
 function DeleteProductModal(props) {
   const [open, setOpen] = React.useState(false);
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.products.loading);
+  const error = useSelector((state) => state.products.error);
+  console.log(error);
 
   const handleClickOpen = () => {
+    dispatch(clearError());
+    dispatch(hideLoading());
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleDelete = (id) => {
+    try {
+      dispatch(clearError());
+      dispatch(showLoading());
+      dispatch(deleteProducts(id)).then(() => {
+        if (error.length === 0) {
+          return false;
+        } else {
+          setOpen(false);
+          dispatch(clearError());
+          dispatch(hideLoading());
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useSelector((state) => console.log("state is : ", state));
 
   return (
     <div>
@@ -38,13 +73,19 @@ function DeleteProductModal(props) {
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
+            {error === "" ? null : (
+              <Alert severity="error" className="mb-2">
+                {error}
+              </Alert>
+            )}
             Data produk yang sudah dihapus tidak bisa dikembalikan kembali.
             Pastikan Anda yakin untuk menghapus data produk :
             {props.product.name}.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="danger">
+          {loading ? <CircularProgress className="mr-2" /> : null}
+          <Button onClick={() => handleDelete(props.product.id)} color="danger">
             Hapus
           </Button>
           <Button onClick={handleClose} color="primary" autoFocus>
