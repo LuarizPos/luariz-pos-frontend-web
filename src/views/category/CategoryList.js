ada9d998f22f7a370399ebf9a6869370d976368f
wicaksana94 <wicaksana94@gmail.com>
2020-12-28 22:34:54 +0700

    Add edit category

--- src/views/category/CategoryList.js
+++ src/views/category/CategoryList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Col, ListGroup, ListGroupItem, Row } from "reactstrap";
 import LoadingData from "../../general_components/LoadingData";
+import EditCategoryModal from "./EditCategoryModal";
 import DeleteCategoryModal from "./DeleteCategoryModal";
 import { getCategories } from "../../store/actions/categoriesActions";
 
@@ -27,7 +28,7 @@ function CategoryList() {
             {categories.map((category) => (
               <ListGroupItem
                 className="shadow-sm p-3 mb-3 bg-white rounded"
-                key={category.id}
+                key={category.id_category}
               >
                 <Row>
                   <Col className="text-left align-self-center">
@@ -37,6 +38,7 @@ function CategoryList() {
                     <small>{category.total_product} Products</small>
                   </Col>
                   <Col className="text-right align-self-center">
+                    <EditCategoryModal category={category} />
                     <DeleteCategoryModal category={category} />
                   </Col>
                 </Row>
--- src/views/category/EditCategoryModal.js
+++ src/views/category/EditCategoryModal.js
@@ -1,11 +1,140 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  FormGroup,
+  Input,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  Label,
+} from "reactstrap";
+import CreateIcon from "@material-ui/icons/Create";
+import Alert from "@material-ui/lab/Alert";
+import {
+  editCategories,
+  showLoading,
+  hideLoading,
+  showError,
+  clearError,
+} from "../../store/actions/categoriesActions";
+import { useDispatch, useSelector } from "react-redux";
+import { CircularProgress } from "@material-ui/core";
 
-function EditCategoryModal() {
-    return (
-        <div>
-            
-        </div>
-    )
+function EditCategoryModal(props) {
+  const dispatch = useDispatch();
+  const [disabledButton, setDisabledButton] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState(props.category.name);
+  const [id] = useState(props.category.id);
+
+  const error = useSelector((state) => state.categories.error);
+  const loadingCategoryData = useSelector(
+    (state) => state.categories.loadingCategoryData
+  );
+
+  function handleOpen() {
+    dispatch(clearError());
+    dispatch(hideLoading());
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  const validateForm = () => {
+    if (name.length === 0) {
+      dispatch(showError("Nama harus diisi"));
+      setDisabledButton(false);
+      return false;
+    } else {
+      dispatch(clearError());
+      return true;
+    }
+  };
+
+  function editCategory(data) {
+    if (validateForm()) {
+      try {
+        const formData = {
+          id_product: id,
+          name,
+        };
+        dispatch(showLoading());
+        setDisabledButton(true);
+        dispatch(editCategories(formData)).then(() => {
+          dispatch(hideLoading());
+
+          // Check if error is exist by get its class name from <Alert> component
+          let errorEditProduct = document.getElementsByClassName(
+            "error-category"
+          );
+
+          // Check errorEditProduct is exist or not
+          if (errorEditProduct.length === 0) {
+            return true;
+          } else {
+            setDisabledButton(false);
+            return false;
+          }
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  return (
+    <>
+      <Modal isOpen={open} fade={false}>
+        {error === "" ? null : (
+          <Alert severity="error" className="error-category m-2">
+            {error}
+          </Alert>
+        )}
+        <ModalHeader>Edit {props.category.name}</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label>Nama Kategori</Label>
+              <Input type="text" onChange={handleChange} id="name" />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          {loadingCategoryData ? <CircularProgress className="ml-2" /> : null}
+          <Button
+            color="primary"
+            disabled={disabledButton}
+            onClick={() => editCategory()}
+            autoFocus
+          >
+            Simpan
+          </Button>
+          <Button
+            color="secondary"
+            onClick={handleClose}
+            disabled={disabledButton}
+          >
+            Batal
+          </Button>
+        </ModalFooter>
+      </Modal>
+      <Button
+        className="border bg-warning rounded-circle p-3 m-2"
+        onClick={handleOpen}
+      >
+        <CreateIcon />
+      </Button>
+    </>
+  );
 }
 
-export default EditCategoryModal
+export default EditCategoryModal;
